b31eef4d0cd6ad2371e8769282bab7a0f113232c
MOHAN BABU SR <writeto.mohanbabu@gmail.com>
2025-02-28 14:29:12 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://time-difference-calculator-be.onrender.com',
+    apiUrl: 'https://time-difference-calculator-be.onrender.com/api',
     firebaseConfig: {
       apiKey: 'PROD_API_KEY',
       authDomain: 'prod-example.firebaseapp.com',
