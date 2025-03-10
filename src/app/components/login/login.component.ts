4ae805b8bed0f596880b2a75aeb24650cba57901
MOHAN BABU SR <writeto.mohanbabu@gmail.com>
2025-03-10 16:46:37 +0530

    profile view updated

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  
+  userData: any;
+
+  ngOnInit(): void {
+    this.userData = {
+      name: 'Mohan Babu S R',
+      userName: 'mohan-admin',
+      email: 'mohan-admin@gmail.com',
+      phoneNumber: '+91 95666 24995',
+      accountStatus: 'Active',
+      lastLogin: moment().format('YYYY-MM-DD HH:mm:ss')
+    }
+  }
 
 }
